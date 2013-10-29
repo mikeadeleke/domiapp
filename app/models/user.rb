6b4e7168fbdd36346e2e12a50c5c1768cc8b576b
Mike Adeleke <madeleke13@gmail.com>
2013-10-28 22:02:55 -0500

    Made some stylting changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 6 }
+  
+  def feed 
+    Properties
+  end
+
   private
   
     def create_remember_token
